500955948c116af018822dae176361bc0709aeca
Chikeobi <chikeobi.njaka@gmail.com>
2020-08-21 15:01:18 -0700

    Syncing project to repo

---
+++ server.js
@@ -0,0 +1,22 @@
+// @see https://github.com/eunahk92/Fitness-Tracker
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+
+let PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/html-routes.js"));
+
+app.listen(PORT, () => console.log(`App running on localhost:${PORT}`));
